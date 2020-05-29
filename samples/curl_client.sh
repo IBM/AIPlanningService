438f1d9e25de356489cf5f27c949ccddc1ffff58
Michael Katz <michael.katz1@ibm.com>
2020-05-28 23:14:27 -0400

    modified shell script to remove comments

--- samples/curl_client.sh
+++ samples/curl_client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-domain=`tr -d '\n' < $1`
-problem=`tr -d '\n' < $2`
+
+domain=`sed 's/;/\n;/g' $1 | sed '/^;/d' | tr -d '\n'`
+problem=`sed 's/;/\n;/g' $2 | sed '/^;/d' | tr -d '\n'`
 body="{\"domain\": \"$domain\", \"problem\": \"$problem\", \"numplans\":$3}"
 basebody=`echo $body`
 curl -d "$basebody" -H "Content-Type: application/json" http://localhost:4501/planners/topk
