fbc17f4a318461fc21096a0de32ce343e3eeaa67
Michael Katz <michael.katz1@ibm.com>
2020-08-14 01:08:38 -0400

    modifications to the docker file, additional python script

--- samples/python_client_topq.py
+++ samples/python_client_topq.py
@@ -3,7 +3,7 @@
 import sys
 import json
 
-def invoke_planner_service(domainfile, problemfile, k, category, planner_name):
+def invoke_planner_service(domainfile, problemfile, k, q, category, planner_name):
     import requests
 
     post_url = 'http://localhost:4501/planners/' + category + '/' + planner_name
@@ -11,6 +11,7 @@ def invoke_planner_service(domainfile, problemfile, k, category, planner_name):
         task = {}
         task["domain"] = d.read()
         task["numplans"] = int(k)
+        task["qualitybound"] = q
         with open(problemfile, "r") as p:
             task["problem"] = p.read()
 
@@ -22,19 +23,10 @@ def invoke_planner_service(domainfile, problemfile, k, category, planner_name):
 
 
 if __name__ == "__main__":
-    category = "topk"
-    planner_name = "kstar-topk"
-    planner_name = "iterative-topk"
+    category = "topq"
+    planner_name = "iterative-unordered-topq"
+    # planner_name = "kstar-topq"
 
-    # category = "optimal"
-    # planner_name = "delfi1"
-
-    category = "satisficing"
-    planner_name = "seq-sat-cerberus"
-
-    # category = "agile"
-    # planner_name = "seq-agl-cerberus"
-
-    ret = invoke_planner_service(sys.argv[1], sys.argv[2], int(sys.argv[3]), category, planner_name)
+    ret = invoke_planner_service(sys.argv[1], sys.argv[2], int(sys.argv[3]), float(sys.argv[4]), category, planner_name)
     
     print(json.dumps(ret, sort_keys=True, indent=4, separators=(',', ': ')))
